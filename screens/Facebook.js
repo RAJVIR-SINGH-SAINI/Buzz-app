3d0dcdfa177df708adb894b141f1bba3687c178f
RAJVIR SINGH SAINI <mannat.saini0431@gmail.com>
2021-06-16 15:33:05 +0530

    Add files via upload

---
+++ screens/Facebook.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text,View,StyleSheet} from 'react-native';
+
+export default class Facebook extends React.Component{
+    render(){
+        return(
+           <Text>Welcome To Facebook</Text> 
+        )
+    }
+}
---
+++ screens/Instagram.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text,View,StyleSheet} from 'react-native';
+
+export default class Instagram extends React.Component{
+    render(){
+        return(
+            <Text>Welcome To Instagram</Text>
+        )
+    }
+}
